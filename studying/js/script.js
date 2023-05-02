24df4c268dfa6615faa465c2a15878a8ef3762bd
ImJordanNetz <jonetz@eastsideprep.org>
2023-05-02 12:57:04 -0700

    Added Two More Pages (No content) and made dark/light into a file script.js

---
+++ studying/js/script.js
@@ -0,0 +1,19 @@
+function change_darklight(){
+    let image = document.getElementById("darklight");
+    var root = document.documentElement;
+    if (image.getAttribute('data') === '1') {
+        image.src = './images/sun.png';
+        image.setAttribute('data', '0');
+        root.style.setProperty('--text', '#ffffff');
+        root.style.setProperty('--bckgrnd', 'rgb(30, 30, 30)');
+        root.style.setProperty('--primary', '#00C5C8');
+        root.style.setProperty('--secondary', '#d4112b');
+    } else {
+        image.src = './images/moon.png';
+        image.setAttribute('data', '1');
+        root.style.setProperty('--text', '#000000');
+        root.style.setProperty('--bckgrnd', '#ffffff');
+        root.style.setProperty('--primary', '#0b6909');
+        root.style.setProperty('--secondary', '#f00a88');
+    }
+}
