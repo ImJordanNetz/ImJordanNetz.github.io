6bf61cbe5fe3ee5dcffa1a7905db877856c8ee44
ImJordanNetz <jonetz@eastsideprep.org>
2023-05-02 22:00:06 -0700

    Added Trailer

--- studying/js/script.js
+++ studying/js/script.js
@@ -17,3 +17,22 @@ function change_darklight(){
         root.style.setProperty('--secondary', '#f00a88');
     }
 }
+
+// Code taken from https://codepen.io/Hyperplexed/pen/abYJQxP
+
+const trailer = document.getElementById("trailer");
+
+window.onmousemove = e => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px)`
+  }
+  
+  trailer.animate(keyframes, { 
+    duration: 3000, 
+    fill: "forwards" 
+  });
+}
+
